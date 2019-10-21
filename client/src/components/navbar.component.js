f299295dfba0fadc5c82c8507caf18e3765f037c
Arely Guevara <arelyguevara@Administrators-MacBook-Pro.local>
2019-10-21 10:58:02 -0400

    added files for database

---
+++ client/src/components/navbar.component.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar(){ 
+    return (
+      <div className="container-fluid">
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <Link to="/" className="navbar-brand">LifeSports</Link>
+        <div className="collpase navbar-collapse">
+        <ul className="navbar-nav mr-auto">
+          <li className="navbar-item">
+          <Link to="/" className="nav-link">Exercises</Link>
+          </li>
+          <li className="navbar-item">
+          <Link to="/create" className="nav-link">Post New Workout</Link>
+          </li>
+          <li className="navbar-item">
+          <Link to="/user" className="nav-link">Create User</Link>
+          </li>
+        </ul>
+        </div>
+      </nav>
+      </div>
+    );  
+}
+export default Navbar
--- server.js
+++ server.js
@@ -1,2 +1,54 @@
-const express = require('express')
-const app = express()
+////////////////////////////////
+////     Road to Hire      ////
+///     LifeSports App     ///
+/////////////////////////////
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require("morgan");
+
+const app = express();
+const port = process.env.PORT || 5000;
+let uri = ""
+
+// register middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Serve up static assets (heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  uri = process.env.ATLAS_URI  // connection string for Atlas here  
+} else {
+  uri = process.env.LOCAL_URI   // connection string for localhost mongo here  
+}
+
+// connection to database
+mongoose.connect(uri, { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true 
+});
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB connection is live");
+})
+
+// register api catalogue
+const exercisesRouter = require('./routes/exercises');
+const usersRouter =     require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+// Creating live connection to reactjs app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
