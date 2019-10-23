2bebae6f3b4ddf446e55857fab5d1149fa9623d0
Ladaisha Pettus <ladaishapettus@Administrators-MacBook-Pro.local>
2019-10-23 10:28:09 -0400

    changed files in server

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './index.css';
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Navbar from "./components/navbar.component"
 import ExercisesList from "./components/exercises-list.component";
@@ -12,14 +12,12 @@ import CreateUser from "./components/create-user.component";
 function App() {
   return (
     <Router>
-      <div className="container-fluid">
       <Navbar />
-      <br/>
+      <br />
       <Route path="/" exact component={ExercisesList} />
       <Route path="/edit/:id" component={EditExercise} />
       <Route path="/create" component={CreateExercise} />
       <Route path="/user" component={CreateUser} />
-      </div>
     </Router>
   );
 }
